066abbbe812fed45defafa376ffd4cecefa1e3e9
KevenMoore <moore_keven@students.columbusstate.edu>
2026-01-26 22:07:09 -0500

    Create First.java

---
+++ First.java
@@ -0,0 +1,11 @@
+// Keven Moore Jr.
+// Computer Science II
+// Spring 2026
+public class First {
+    public static void main(String[] args) {
+        System.out.println("Hola mundo!");
+        System.out.println("Job title: Software Developer");
+        System.out.println("Why: I'm interested in this job because I would like to crearte my own game");
+        System.out.println("Median salary: $133,000");
+    }
+}
