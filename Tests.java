37e6f7c9e88335de13fcd322d7d38ad3202736e3
KevenMoore <moore_keven@students.columbusstate.edu>
2026-02-07 10:46:08 -0500

    Add files via upload

---
+++ Tests.java
@@ -0,0 +1,18 @@
+// Name: Keven Moore
+// This program stores three test scores and prints their average.
+
+public class Tests {
+    public static void main(String[] args) {
+
+        double test1 = 88.2;
+        double test2 = 78.9;
+        double test3 = 97.6;
+
+        double average = (test1 + test2 + test3) / 3;
+
+        System.out.println("Test score 1:   " + test1);
+        System.out.println("Test score 2:   " + test2);
+        System.out.println("Test score 3:   " + test3);
+        System.out.println("The average of 3 test scores is:  " + average);
+    }
+}
